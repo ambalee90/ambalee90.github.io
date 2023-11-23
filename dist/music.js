8b00a286adecb9615e6c5274388eefaa0c2ca7d4
Amber Lee <amberlee07286@gmail.com>
2023-11-23 20:16:43 +0800

    Site updated: 2023-11-23 20:16:40

---
+++ dist/music.js
@@ -0,0 +1,22 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: false,
+    autoplay: false,
+    theme: '#b7daff',
+    loop: 'all',
+    order: 'list',
+    preload: 'auto',
+    volume: 0.7,
+    mutex: true,
+    listFolded: false,
+    listMaxHeight: '200px',
+    lrcType: 3,
+    audio: [
+      {
+        name: "Music",
+        artist: '兒歌',
+        url: 'images/Music.mp3',
+        cover: '/music/cover/兩隻老虎.jpg'
+      }
+    ]
+});
